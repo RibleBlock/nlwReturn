569c0c107d2f559cd7e6d7409f1908bffe492d7f
Riquelme <riquelmeaparecido08@gmail.com>
2022-05-23 10:08:38 -0300

    teste de funcao com spy

--- backend/src/services/submitFeedback.services.spec.ts
+++ backend/src/services/submitFeedback.services.spec.ts
@@ -5,18 +5,24 @@ test('sum 2 + 2', () => {
 });
 
 // JEST
-describe('Submit feedback', () => {
-  const submitFeedback = new SubmitFeedbackServices(
-    { create: async () => {} }, // Dependencia mocadas
-    { sendMail: async () => {} }, // Dependencia mocadas
-  );
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
 
+const submitFeedback = new SubmitFeedbackServices(
+  { create: createFeedbackSpy }, // Dependencia mocadas
+  { sendMail: sendMailSpy }, // Dependencia mocadas
+);
+
+describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
     await expect(submitFeedback.execute({
       type: 'BUG',
       comment: 'example comment',
       screenshot: 'data:image/png;base64,ewfwefwef',
     })).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
   });
 
   it('should not be able to submit a feedback without type', async () => {
